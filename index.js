b2975e20dd099cbcd265d075d20e19c1534a6f8c
Ritesh Mehrotra <Ritesh_Mehrotra@Singaporeair.com.sg>
2020-05-06 17:27:01 +0800

    Added default path

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const PORT = process.env.PORT || 3000;
 
 const app  = express();
 
+app.get("/",(req,res)=>{
+    res.send("This is TechTalks portal");
+})
+
 app.get("/hello",(req, res)=>{
     res.send("Hello! Welcome to Techtalks world");
 })
